76aaf69817132dd76c92df65763e320df0029741
Shogo Sakihama <goshalways@gmail.com>
2021-01-14 00:01:00 +0900

    ソートを修正

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
       $user = Auth::id();
       $posts = Post::take(10)
       ->with('like')
-      ->orderBy('updated_at', 'desc')
+      ->orderBy('id', 'desc')
       ->get();
 
       $res = [
