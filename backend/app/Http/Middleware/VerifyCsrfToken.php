52175f33a5e9bcfe5742b3d7507247a6b3c5ee4d
Shogo Sakihama <goshalways@gmail.com>
2021-01-15 17:40:38 +0900

    csrf対策

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,10 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'http://127.0.0.1:10080/api/*',
+        '*',
         'http://127.0.0.1:10080/api/like',
-        // 'http://127.0.0.1:10080/api/logout',
-        // 'http://127.0.0.1:10080/api/post',
-        // 'http://127.0.0.1:10080/api/remove',
+        'http://127.0.0.1:10080/api/login',
     ];
 }
